d668d866ba7a2fc4539216aa8b6a8f2ace9d3e24
Richard Doe <richard.doe@rwdit.net>
2020-07-15 11:05:59 +0100

    Docker Compose with SMTP and europeana realm

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM jboss/keycloak
 
+ARG config=/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml
+
+COPY europeana-realm.json /tmp
+ENV KEYCLOAK_IMPORT=/tmp/europeana-realm.json
+
 # Disable caching for themes
-
-ADD ./standalone-ha.xml /opt/jboss/keycloak/standalone/configuration/standalone-ha.xml
-
-
+RUN sed -ir 's/<staticMaxAge>[^<]*<\/staticMaxAge>/<staticMaxAge>-1<\/staticMaxAge>/' ${config} && \
+  sed -ir 's/<cacheThemes>[^<]*<\/cacheThemes>/<cacheThemes>false<\/cacheThemes>/' ${config} && \
+  sed -ir 's/<cacheTemplates>[^<]*<\/cacheTemplates>/<cacheTemplates>false<\/cacheTemplates>/' ${config}
--- devbuild.sh
+++ devbuild.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -o errexit
-
-# stop container keycloaktheme if exists
-# docker stop keycloaktheme || true && docker  rm -f keycloaktheme || true && docker rmi europeana/keycloak-theme || true
 
 echo copying bootstrap and jquery to theme resources
 mkdir -p theme/login/resources/js
@@ -10,21 +7,9 @@ cp -R node_modules/bootstrap/dist/js/bootstrap.min.* theme/login/resources/js/
 cp -R node_modules/jquery/dist/jquery.min.* theme/login/resources/js/
 cp -R node_modules/@europeana/portal/assets assets
 
-
 echo creating css file
-npx sass --load-path=./node_modules --load-path=./assets/scss custom/scss/style.scss:theme/login/resources/css/portallogin.css --update
+npx sass --load-path=./node_modules --load-path=./assets/scss \
+  custom/scss/style.scss:theme/login/resources/css/portallogin.css --update
 
-
-echo building image
-docker build -t europeana/keycloak-theme .
-
-echo starting image
-docker container run -i --name keycloaktheme \
-  -p 10001:8080 \
-  --mount type=bind,source="$(pwd)"/theme,target=/opt/jboss/keycloak/themes/europeana \
-  -v "$(pwd)":/europeana \
-  -e KEYCLOAK_USER=admin \
-  -e KEYCLOAK_PASSWORD=password europeana/keycloak-theme
-
-
-
+echo starting Docker Compose
+docker-compose up --build
