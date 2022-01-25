c050090ae0a10d9c29619d84e237fd7382ce5975
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2022-01-25 16:02:21 +0100

    Revert linting

--- theme/account/resources/content/account-page/AccountPage.js
+++ theme/account/resources/content/account-page/AccountPage.js
@@ -1,44 +1,37 @@
-function _defineProperty(obj, key, value) {
-  if (key in obj) {
-    Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
-  } else {
-    obj[key] = value;
-  }
-  return obj;
-}
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 /*
  * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the 'License');
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-/*
+ /*
   * This file is taken from the jboss/keycloak:12.0.4 docker image file:
   * themes/keycloak.v2/account/resources/content/account-page/AccountPage.js
   *
   * It has been modified to remove the First and Last name fields.
   * Specifically see the section commented: EUROPEANA EDIT
   */
-import * as React from '../../../../common/keycloak/web_modules/react.js';
-import { ActionGroup, Button, Form, FormGroup, TextInput, Grid, GridItem, Expandable } from '../../../../common/keycloak/web_modules/@patternfly/react-core.js';
-import { AccountServiceContext } from '../../account-service/AccountServiceContext.js';
-import { Msg } from '../../widgets/Msg.js';
-import { ContentPage } from '../ContentPage.js';
-import { ContentAlert } from '../ContentAlert.js';
-import { LocaleSelector } from '../../widgets/LocaleSelectors.js';
-import { KeycloakContext } from '../../keycloak-service/KeycloakContext.js';
-import { AIACommand } from '../../util/AIACommand.js';
+import * as React from "../../../../common/keycloak/web_modules/react.js";
+import { ActionGroup, Button, Form, FormGroup, TextInput, Grid, GridItem, Expandable } from "../../../../common/keycloak/web_modules/@patternfly/react-core.js";
+import { AccountServiceContext } from "../../account-service/AccountServiceContext.js";
+import { Msg } from "../../widgets/Msg.js";
+import { ContentPage } from "../ContentPage.js";
+import { ContentAlert } from "../ContentAlert.js";
+import { LocaleSelector } from "../../widgets/LocaleSelectors.js";
+import { KeycloakContext } from "../../keycloak-service/KeycloakContext.js";
+import { AIACommand } from "../../util/AIACommand.js";
 
 /**
  * @author Stan Silvert ssilvert@redhat.com (C) 2018 Red Hat Inc.
@@ -47,15 +40,15 @@ export class AccountPage extends React.Component {
   constructor(props, context) {
     super(props);
 
-    _defineProperty(this, 'context', void 0);
+    _defineProperty(this, "context", void 0);
 
-    _defineProperty(this, 'isRegistrationEmailAsUsername', features.isRegistrationEmailAsUsername);
+    _defineProperty(this, "isRegistrationEmailAsUsername", features.isRegistrationEmailAsUsername);
 
-    _defineProperty(this, 'isEditUserNameAllowed', features.isEditUserNameAllowed);
+    _defineProperty(this, "isEditUserNameAllowed", features.isEditUserNameAllowed);
 
-    _defineProperty(this, 'isDeleteAccountAllowed', features.deleteAccountAllowed);
+    _defineProperty(this, "isDeleteAccountAllowed", features.deleteAccountAllowed);
 
-    _defineProperty(this, 'DEFAULT_STATE', {
+    _defineProperty(this, "DEFAULT_STATE", {
       errors: {
         username: '',
         firstName: '',
@@ -71,13 +64,13 @@ export class AccountPage extends React.Component {
       }
     });
 
-    _defineProperty(this, 'state', this.DEFAULT_STATE);
+    _defineProperty(this, "state", this.DEFAULT_STATE);
 
-    _defineProperty(this, 'handleCancel', () => {
+    _defineProperty(this, "handleCancel", () => {
       this.fetchPersonalInfo();
     });
 
-    _defineProperty(this, 'handleChange', (value, event) => {
+    _defineProperty(this, "handleChange", (value, event) => {
       const target = event.currentTarget;
       const name = target.name;
       this.setState({
@@ -90,7 +83,7 @@ export class AccountPage extends React.Component {
       });
     });
 
-    _defineProperty(this, 'handleSubmit', event => {
+    _defineProperty(this, "handleSubmit", event => {
       event.preventDefault();
       const form = event.target;
       const isValid = form.checkValidity();
@@ -98,7 +91,7 @@ export class AccountPage extends React.Component {
       if (isValid) {
         const reqData = { ...this.state.formFields
         };
-        this.context.doPost('/', reqData).then(() => {
+        this.context.doPost("/", reqData).then(() => {
           ContentAlert.success('accountUpdatedMessage');
 
           if (locale !== this.state.formFields.attributes.locale[0]) {
@@ -119,26 +112,26 @@ export class AccountPage extends React.Component {
       }
     });
 
-    _defineProperty(this, 'handleDelete', keycloak => {
-      new AIACommand(keycloak, 'delete_account').execute();
+    _defineProperty(this, "handleDelete", keycloak => {
+      new AIACommand(keycloak, "delete_account").execute();
     });
 
-    _defineProperty(this, 'UsernameInput', () => React.createElement(TextInput, {
+    _defineProperty(this, "UsernameInput", () => React.createElement(TextInput, {
       isRequired: true,
-      type: 'text',
-      id: 'user-name',
-      name: 'username',
+      type: "text",
+      id: "user-name",
+      name: "username",
       maxLength: 254,
       value: this.state.formFields.username,
       onChange: this.handleChange,
       isValid: this.state.errors.username === ''
     }));
 
-    _defineProperty(this, 'RestrictedUsernameInput', () => React.createElement(TextInput, {
+    _defineProperty(this, "RestrictedUsernameInput", () => React.createElement(TextInput, {
       isDisabled: true,
-      type: 'text',
-      id: 'user-name',
-      name: 'username',
+      type: "text",
+      id: "user-name",
+      name: "username",
       value: this.state.formFields.username
     }));
 
@@ -147,7 +140,7 @@ export class AccountPage extends React.Component {
   }
 
   fetchPersonalInfo() {
-    this.context.doGet('/').then(response => {
+    this.context.doGet("/").then(response => {
       this.setState(this.DEFAULT_STATE);
       const formFields = response.data;
 
@@ -160,48 +153,49 @@ export class AccountPage extends React.Component {
       }
 
       this.setState({ ...{
-        formFields: formFields
-      } });
+          formFields: formFields
+        }
+      });
     });
   }
 
   render() {
     const fields = this.state.formFields;
     return React.createElement(ContentPage, {
-      title: 'personalInfoHtmlTitle',
-      introMessage: 'personalSubMessage'
+      title: "personalInfoHtmlTitle",
+      introMessage: "personalSubMessage"
     }, React.createElement(Form, {
       isHorizontal: true,
       onSubmit: event => this.handleSubmit(event)
     }, !this.isRegistrationEmailAsUsername && React.createElement(FormGroup, {
       label: Msg.localize('username'),
       isRequired: true,
-      fieldId: 'user-name',
+      fieldId: "user-name",
       helperTextInvalid: this.state.errors.username,
       isValid: this.state.errors.username === ''
     }, this.isEditUserNameAllowed && React.createElement(this.UsernameInput, null), !this.isEditUserNameAllowed && React.createElement(this.RestrictedUsernameInput, null)), React.createElement(FormGroup, {
       label: Msg.localize('email'),
       isRequired: true,
-      fieldId: 'email-address',
+      fieldId: "email-address",
       helperTextInvalid: this.state.errors.email,
       isValid: this.state.errors.email === ''
     }, React.createElement(TextInput, {
       isRequired: true,
-      type: 'email',
-      id: 'email-address',
-      name: 'email',
+      type: "email",
+      id: "email-address",
+      name: "email",
       maxLength: 254,
       value: fields.email,
       onChange: this.handleChange,
       isValid: this.state.errors.email === ''
     })),
     // EUROPEANA EDIT - REMOVED FIRST & LAST NAME INPUTS FROM HERE
-    features.isInternationalizationEnabled && React.createElement(FormGroup, {
+     features.isInternationalizationEnabled && React.createElement(FormGroup, {
       label: Msg.localize('selectLocale'),
       isRequired: true,
-      fieldId: 'locale'
+      fieldId: "locale"
     }, React.createElement(LocaleSelector, {
-      id: 'locale-selector',
+      id: "locale-selector",
       value: fields.attributes.locale || '',
       onChange: value => this.setState({
         errors: this.state.errors,
@@ -212,45 +206,48 @@ export class AccountPage extends React.Component {
         }
       })
     })), React.createElement(ActionGroup, null, React.createElement(Button, {
-      type: 'submit',
-      id: 'save-btn',
-      variant: 'primary',
+      type: "submit",
+      id: "save-btn",
+      variant: "primary",
       isDisabled: Object.values(this.state.errors).filter(e => e !== '').length !== 0
     }, React.createElement(Msg, {
-      msgKey: 'doSave'
+      msgKey: "doSave"
     })), React.createElement(Button, {
-      id: 'cancel-btn',
-      variant: 'secondary',
+      id: "cancel-btn",
+      variant: "secondary",
       onClick: this.handleCancel
     }, React.createElement(Msg, {
-      msgKey: 'doCancel'
-    })))), this.isDeleteAccountAllowed && React.createElement('div', {
-      id: 'delete-account',
+      msgKey: "doCancel"
+    })))), this.isDeleteAccountAllowed && React.createElement("div", {
+      id: "delete-account",
       style: {
-        marginTop: '30px'
+        marginTop: "30px"
       }
     }, React.createElement(Expandable, {
-      toggleText: 'Delete Account'
+      toggleText: "Delete Account"
     }, React.createElement(Grid, {
-      gutter: 'sm'
+      gutter: "sm"
     }, React.createElement(GridItem, {
       span: 6
-    }, React.createElement('p', null, React.createElement(Msg, {
-      msgKey: 'deleteAccountWarning'
+    }, React.createElement("p", null, React.createElement(Msg, {
+      msgKey: "deleteAccountWarning"
     }))), React.createElement(GridItem, {
       span: 4
     }, React.createElement(KeycloakContext.Consumer, null, keycloak => React.createElement(Button, {
-      id: 'delete-account-btn',
-      variant: 'danger',
+      id: "delete-account-btn",
+      variant: "danger",
       onClick: () => this.handleDelete(keycloak),
-      className: 'delete-button'
+      className: "delete-button"
     }, React.createElement(Msg, {
-      msgKey: 'doDelete'
+      msgKey: "doDelete"
     })))), React.createElement(GridItem, {
       span: 2
     })))));
   }
+
 }
 
-_defineProperty(AccountPage, 'contextType', AccountServiceContext);
-// # sourceMappingURL=AccountPage.js.map
+_defineProperty(AccountPage, "contextType", AccountServiceContext);
+
+;
+//# sourceMappingURL=AccountPage.js.map
